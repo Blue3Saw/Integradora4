272b3fcadc818087eb473dcd9ae00caa0cabfe2d
Saidy <saidy0521@hotmail.com>
2017-11-15 00:21:07 -0600

    xxxx

--- ProyectoUniJob/DAO/ConexionDAO.cs
+++ ProyectoUniJob/DAO/ConexionDAO.cs
@@ -17,14 +17,14 @@ namespace DAO
 
         public SqlConnection ConectarBD()
         {
-<<<<<<< HEAD
-            string cad_con = "Data Source=LAPTOP-JNUSO21H\\SQLEXPRESS; Initial Catalog=Integrador3; Integrated Security=True";
-=======
+
+            //string cad_con = "Data Source=LAPTOP-JNUSO21H\\SQLEXPRESS; Initial Catalog=Integrador3; Integrated Security=True";
+
 
             string cad_con = "Data Source=ALFONSO-PC; Initial Catalog=Integrador3; Integrated Security=True"; ;
             //string cad_con = "Data Source=LAPTOP-80619S9J; Initial Catalog=Integrador3; Integrated Security=True";
 
->>>>>>> 2e1c98ea28edfe73aeec6a85a5617768cce0765b
+
             con = new SqlConnection(cad_con);
             return con;
         }
