1849b4bc42a7be990aae02dd9da7c94d57f33c6d
Blue3Saw <euan.alberto415@gmail.com>
2017-11-19 13:54:52 -0600

    Pruebita

--- ProyectoUniJob/DAO/ConexionDAO.cs
+++ ProyectoUniJob/DAO/ConexionDAO.cs
@@ -18,7 +18,7 @@ namespace DAO
         public SqlConnection ConectarBD()
         {
 
-            string cad_con = "Data Source=LAPTOP-80619S9J; Initial Catalog=Integrador3; Integrated Security=True"; ;
+            string cad_con = "Data Source=LAPTOP-80619S9J; Initial Catalog=Integrador3; Integrated Security=True";
             //string cad_con = "Data Source=LAPTOP-80619S9J; Initial Catalog=Integrador3; Integrated Security=True";
 
             con = new SqlConnection(cad_con);
